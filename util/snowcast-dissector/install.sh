d9db29de5f7d8c39eb8793ef2af6f0c46a16c322
Nicholas DeMarinis <ndemarinis@wpi.edu>
2023-09-14 13:24:26 -0400

    Fixed quoting on dissector install script.

--- util/snowcast-dissector/install.sh
+++ util/snowcast-dissector/install.sh
@@ -7,8 +7,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TARGET=~/.config/wireshark/plugins
 
 main() {
-    mkdir -pv ${TARGET}
-    install -v -m644 ${SCRIPT_DIR}/cs168_snowcast.lua ${TARGET}
+    mkdir -pv "${TARGET}"
+    install -v -m644 "${SCRIPT_DIR}/cs168_snowcast.lua" "${TARGET}"
 }
 
 main $@
